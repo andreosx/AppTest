2c4a735f995790a763f492495d3dede9dfde29ca
André Oliveira <andreosx@gmail.com>
2023-01-14 09:39:49 -0300

    Ajustes de code review.

--- app/src/main/java/com/br/apptest/presenter/adapter/RepoAdapter.kt
+++ app/src/main/java/com/br/apptest/presenter/adapter/RepoAdapter.kt
@@ -46,4 +46,52 @@ class RepoAdapter(val ctx: Context, private val cellClickListener: CellClickList
         }
     }
 
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainActivityAdapterHolder {
+//        return MainActivityAdapterHolder(LayoutInflater.from(parent.context).inflate(R.layout.adapter_repo, parent, false))
+//    }
+//
+//    override fun onBindViewHolder(holder: MainActivityAdapterHolder, position: Int) {
+//        setValues(holder,item[position])
+//        setAcessibility(holder,item[position])
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return item.size
+//    }
+//
+//    class MainActivityAdapterHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//        val binding = AdapterRepoBinding.bind(itemView)
+//    }
+//
+//    fun updateList(Repo: List<Repo>, oldCount: Int, tvShowListSize: Int) {
+//        this.item = Repo
+//        notifyDataSetChanged()
+//        notifyItemRangeInserted(oldCount, tvShowListSize)
+//    }
+//
+//    private fun setValues(holder: MainActivityAdapterHolder, repo: Repo){
+//        holder.binding.repositoryName.text = repo.name
+//        holder.binding.repositoryDescription.text = repo.description
+//        holder.binding.repositoryForkCount.text = repo.forks_count.toString()
+//        holder.binding.repositoryStarCount.text = repo.stargazers_count.toString()
+//        holder.binding.ownerUsername.text = repo.owner.login
+//
+//        Glide.with(ctx)
+//            .load(repo.owner.avatar_url)
+//            .circleCrop()
+//            .into(holder.binding.ownerPicture)
+//
+//        holder.itemView.setOnClickListener {
+//            cellClickListener.onCellClickListener(repo)
+//        }
+//    }
+//
+//    private fun setAcessibility(holder: MainActivityAdapterHolder, repo: Repo){
+//        holder.binding.repositoryName.contentDescription = ctx.getString(R.string.repository_name,repo.name)
+//        holder.binding.repositoryDescription.contentDescription = ctx.getString(R.string.repository_description,repo.description)
+//        holder.binding.repositoryForkCount.contentDescription = ctx.getString(R.string.forks_description,repo.forks_count.toString())
+//        holder.binding.repositoryStarCount.contentDescription = ctx.getString(R.string.star_description,repo.stargazers_count.toString())
+//        holder.binding.ownerUsername.contentDescription = ctx.getString(R.string.owner_description,repo.owner.login)
+//        holder.binding.ownerPicture.contentDescription = ctx.getString(R.string.avatar_owner_description)
+//    }
 }
